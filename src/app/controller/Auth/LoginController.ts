ef53f3cac10f08d02223d00548d27b20c8ee07ee
holiq <holiq.ibrahim376@gmail.com>
2024-11-16 08:28:01 +0700

    feat(auth): implement login

--- src/app/controller/Auth/LoginController.ts
+++ src/app/controller/Auth/LoginController.ts
@@ -1,10 +1,32 @@
-import { Context } from 'hono'
+import { db } from '@/database'
+import { users } from '@/database/schema/user'
+import { encrypt } from '@/utils/JwtHandler'
 import { Response } from '@/utils/Response'
+import bcrypt from 'bcryptjs'
+import { eq } from 'drizzle-orm'
+import { Context } from 'hono'
 
 export class LoginController
 {
   static async handle(c: Context) {
-    const data = await c.req.json()
+    const {email, password} = await c.req.json()
+
+    const userRecords = await db.select().from(users).where(eq(users.email, email))
+
+    const user = userRecords[0]
+
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return Response.resolveForFailed(c, 'Invalid credentials')
+    }
+
+    const data = {
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+      token: encrypt(email),
+    }
 
     return Response.resolveForSuccess(c, 'Login Success', data)
   }
